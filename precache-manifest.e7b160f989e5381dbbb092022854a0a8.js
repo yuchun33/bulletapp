5525a20d13320d653caf68153e9e9d84e1a66272
yuchun33 <yuchunlin33@gmail.com>
2020-02-27 23:02:01 +0800

    Updates

---
+++ precache-manifest.e7b160f989e5381dbbb092022854a0a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "728dbe3a3bb9379825d313d2bc6c247d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9df39e50842ba414ecc1",
+    "url": "/static/css/main.6c14dce8.chunk.css"
+  },
+  {
+    "revision": "7be93f5000117927725a",
+    "url": "/static/js/2.50590421.chunk.js"
+  },
+  {
+    "revision": "6b11c6bdd7cab76bbe5c6e5eff2c46fa",
+    "url": "/static/js/2.50590421.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9df39e50842ba414ecc1",
+    "url": "/static/js/main.1ccef92e.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
